b799eb2bdab6f235587cd6714c062be984ab3bc7
Mikkel Krautz <mikkel@krautz.dk>
2011-11-12 03:37:56 +0100

    Update sync.bash to be *correct*.

--- pkg/mumbleproto/sync.bash
+++ pkg/mumbleproto/sync.bash
@@ -2,4 +2,4 @@
 # Syncs the Mumble.proto file with the main Mumble repo
 
 curl -O https://raw.github.com/mumble-voip/mumble/master/src/Mumble.proto
-sed -i -e 's,MumbleProto,mumbleproto,' Mumble.proto
+sed -i '' -e 's,MumbleProto,mumbleproto,' Mumble.proto
