9d091cc32aeeb389b833587df0a525456c933a4c
Tim Cooper <tim.cooper@layeh.com>
2016-03-27 14:06:07 -0300

    fix proto sync script

--- pkg/mumbleproto/sync.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Syncs the Mumble.proto file with the main Mumble repo
-
-curl -O https://raw.github.com/mumble-voip/mumble/master/src/Mumble.proto
-sed -i '' -e 's,MumbleProto,mumbleproto,' Mumble.proto
